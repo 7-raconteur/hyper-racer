ff43b78af4662168d907bf1d2c705e445f6b1b04
7-raconteur <rnsapdlf@gmail.com>
2025-01-23 15:51:47 +0900

    HR-6 GameManager 상태 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,13 +13,21 @@ public class GameManager : MonoBehaviour
     // UI 관련
     [SerializeField] private MoveButton leftMoveButton;
     [SerializeField] private MoveButton rightMoveButton;
+    [SerializeField] private TMP_Text gasText;
     
     // 도로 오브젝트 풀
     private Queue<GameObject> _roadPool = new Queue<GameObject>();
     private int _roadPoolSize = 3;
     
+    // 자동차
+    private CarController _carController;
+    
     // 도로 이동
     private List<GameObject> _activeRoads = new List<GameObject>();
+    
+    // 상태
+    public enum State { Start, Play, End }
+    public State GameState { get; private set; } = State.Start;
     
     // 싱글턴
     public static GameManager _instance;
@@ -63,6 +72,9 @@ public class GameManager : MonoBehaviour
         {
             activeRoad.transform.Translate(-Vector3.forward * Time.deltaTime);
         }
+        
+        // Gas 정보 출력
+        if(_carController != null) gasText.text = _carController.Gas.ToString();
     }
 
     private void StartGame()
@@ -71,14 +83,12 @@ public class GameManager : MonoBehaviour
         SpawnRoad(Vector3.zero);
         
         // 자동차 생성
-        var carController = Instantiate(carPrefab, new Vector3(0, 0, -3f), Quaternion.identity)
+        var _carController = Instantiate(carPrefab, new Vector3(0, 0, -3f), Quaternion.identity)
             .GetComponent<CarController>();
         
         // left, right move button에 자동차 컨트롤 기능 적용
-        leftMoveButton.OnMoveButtonDwon += () => carController.Move(-1f);
-        rightMoveButton.OnMoveButtonDwon += () => carController.Move(1f);
-        
-
+        leftMoveButton.OnMoveButtonDwon += () => _carController.Move(-1f);
+        rightMoveButton.OnMoveButtonDwon += () => _carController.Move(1f);
     }
     
     // 도로 생성 및 관리
